3b3b06d1a3789743d3ab659b2df22a3d80c7de50
Florian Roth <venom14@gmail.com>
2016-09-02 16:30:55 +0200

    Removed more characters in cleanup

--- fenrir.sh
+++ fenrir.sh
@@ -392,7 +392,7 @@ function log {
     done
 
     # Remove line breaks
-    message=$(echo "$message" | tr '\n' ' ') 
+    message=$(echo "$message" | tr -d '\r' | tr -d '\l' | tr '\n' ' ') 
 
     # Remove prefix (e.g. [+])
     if [[ "${message:0:1}" == "[" ]]; then
